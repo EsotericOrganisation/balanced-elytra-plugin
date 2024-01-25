0c276da4933f3cb884247825577de037c7b10b32
rolyPolyVole <yezhanting@gmail.com>
2024-01-25 21:33:11 +0000

    Initial & probably final commit.

---
+++ src/main/java/org/rolypolyvole/noelytra/NoElytra.java
@@ -0,0 +1,29 @@
+package org.rolypolyvole.noelytra;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoElytra extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onRocketBoost(PlayerInteractEvent event) {
+        boolean usedRocketInWorld =
+            event.getMaterial().equals(Material.FIREWORK_ROCKET) &&
+            event.getPlayer().isGliding() &&
+            event.getPlayer().getWorld().getEnvironment().equals(World.Environment.NORMAL);
+
+        if (usedRocketInWorld) {
+            event.setCancelled(true);
+        }
+    }
+}
